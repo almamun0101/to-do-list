5cafacb85778482f86f4ccc248f63ebc39432fc0
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-02-23 15:20:28 +0600

    ain

--- src/script.js
+++ src/script.js
@@ -1,4 +1,26 @@
 let input = document.getElementById("input_value")
-input.onkeypress = function(e) {
-    if(e=="Enter"){console.log("mamun")}
+let ul = document.getElementById("list")
+input.onkeypress = function (e) {
+    if (e == "Enter") {
+        console.log("mamun")
+    }
 };
+input.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+        create_list()
+    }
+});
+
+function create_list(){
+    let i_value = input.value
+    let li = document.createElement("li")
+    let text = document.createTextNode(i_value)
+    let text2 = document.createTextNode("x")
+    li.className = "list-style"
+    li.appendChild(text)
+    li.appendChild(text2)
+    ul.appendChild(li)
+    input.value = ""
+    // console.log(i_value)
+  
+}

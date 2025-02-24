2e93be37e026b373207a5fb67e128b818d83d66c
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-02-24 13:11:47 +0600

    ain

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 let input = document.getElementById("input_value")
+let totall = document.getElementById("count_list")
+let complete = document.getElementById("count_done")
+let left = document.getElementById("count_left")
+
 let ul = document.getElementById("list")
 input.onkeypress = function (e) {
     if (e == "Enter") {
@@ -16,25 +20,42 @@ function create_list(){
     let li = document.createElement("li")
     let span = document.createElement("span")
     let icon = document.createElement("i")
-    let text = document.createTextNode(i_value)
-    let btn = 
+    let t = i_value.charAt(0).toUpperCase() + i_value.slice(1) 
+    let text = document.createTextNode(t)
+    if(!i_value){
+        return
+    }
 
     icon.className = "fa-solid fa-check"
     li.className = "list-style"
-
+    
     li.appendChild(text)
     span.appendChild(icon)
     li.appendChild(span)
     ul.appendChild(li)
     input.value = ""
-    li.addEventListener("dblclick", ()=>{
-        li.className = "list-done"
+
+    counts()
+
+    li.addEventListener("click", ()=>{
+        li.className += " list-done"
         icon.className = "fa-solid fa-trash"
+        counts()
     })
     
     icon.addEventListener("dblclick",()=>{
         li.parentNode.removeChild(li);
+        counts()
     })
-    // console.log(i_value)
-  
+    
+}
+
+function counts(){
+    let list_length = ul.getElementsByClassName("list-style").length
+    let done_length = ul.getElementsByClassName("list-done").length
+
+    totall.innerHTML = list_length
+    complete.innerHTML = done_length
+    left.innerHTML = list_length - done_length;
+    
 }
